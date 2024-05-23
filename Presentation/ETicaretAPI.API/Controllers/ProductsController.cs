00e232297d728218c5efd7a04f0c71b3f3e2cc5b
Eren OĞUZ <92570103+Trustedaid@users.noreply.github.com>
2024-05-24 01:15:11 +0300

    Uploading File

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -6,84 +6,108 @@ using ETicaretAPI.Application.ViewModels.Products;
 using ETicaretAPI.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ETicaretAPI.API.Controllers
+namespace ETicaretAPI.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase // TEST CONTROLLER
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase // TEST CONTROLLER
+    private readonly IProductWriteRepository _productWriteRepository;
+    private readonly IProductReadRepository _productReadRepository;
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+
+    public ProductsController(IProductWriteRepository productWriteRepository,
+        IProductReadRepository productReadRepository, IWebHostEnvironment webHostEnvironment)
     {
-        private readonly IProductWriteRepository _productWriteRepository;
-        private readonly IProductReadRepository _productReadRepository;
+        _productWriteRepository = productWriteRepository;
+        _productReadRepository = productReadRepository;
+        _webHostEnvironment = webHostEnvironment;
+    }
 
-
-        public ProductsController(IProductWriteRepository productWriteRepository,
-            IProductReadRepository productReadRepository)
-        {
-            _productWriteRepository = productWriteRepository;
-            _productReadRepository = productReadRepository;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Get([FromQuery] Pagination pagination)
-        {
-            
-            var totalCount = _productReadRepository.GetAll(false).Count();
-            var products = _productReadRepository.GetAll(false)
-                .Skip(pagination.Page * pagination.Size).Take(pagination.Size)
-                .Select(x => new
-                {
-                    x.Id,
-                    x.Name,
-                    x.Stock,
-                    x.Price,
-                    x.CreatedDate,
-                    x.UpdatedDate
-                }).ToList();
-            return Ok(new
+    [HttpGet]
+    public async Task<IActionResult> Get([FromQuery] Pagination pagination)
+    {
+        var totalCount = _productReadRepository.GetAll(false).Count();
+        var products = _productReadRepository.GetAll(false)
+            .Skip(pagination.Page * pagination.Size).Take(pagination.Size)
+            .Select(x => new
             {
-                totalCount,
-                products
-            });
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get(string id)
+                x.Id,
+                x.Name,
+                x.Stock,
+                x.Price,
+                x.CreatedDate,
+                x.UpdatedDate
+            }).ToList();
+        return Ok(new
         {
-            return Ok(await _productReadRepository.GetByIdAsync(id, false));
-        }
+            totalCount,
+            products
+        });
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> Post(VMCreateProduct model)
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(string id)
+    {
+        return Ok(await _productReadRepository.GetByIdAsync(id, false));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(VMCreateProduct model)
+    {
+        await _productWriteRepository.AddAsync(new()
         {
-            await _productWriteRepository.AddAsync(new()
-            {
-                Name = model.Name,
-                Price = model.Price,
-                Stock = model.Stock
-            });
-            await _productWriteRepository.SaveAsync();
-            return StatusCode((int)HttpStatusCode.Created);
-        }
+            Name = model.Name,
+            Price = model.Price,
+            Stock = model.Stock
+        });
+        await _productWriteRepository.SaveAsync();
+        return StatusCode((int)HttpStatusCode.Created);
+    }
 
-        [HttpPut]
-        public async Task<IActionResult> Put(VMUpdateProduct model)
+    [HttpPut]
+    public async Task<IActionResult> Put(VMUpdateProduct model)
+    {
+        Product product = await _productReadRepository.GetByIdAsync(model.Id);
+        product.Stock = model.Stock;
+        product.Name = model.Name;
+        product.Price = model.Price;
+
+        await _productWriteRepository.SaveAsync();
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        await _productWriteRepository.RemoveAsync(id);
+        await _productWriteRepository.SaveAsync();
+
+        return Ok();
+    }
+
+    [HttpPost("[action]")]
+    public async Task<IActionResult> Upload()
+    {
+        var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "resource/product-images");
+
+        if (!Directory.Exists(uploadPath))
+            Directory.CreateDirectory(uploadPath);
+
+        Random random = new();
+
+        foreach (var file in Request.Form.Files)
         {
-            Product product = await _productReadRepository.GetByIdAsync(model.Id);
-            product.Stock = model.Stock;
-            product.Name = model.Name;
-            product.Price = model.Price;
+            var fullPath = Path.Combine(uploadPath, $"{random.Next()}{Path.GetExtension(file.FileName)}");
 
-            await _productWriteRepository.SaveAsync();
-            return Ok();
+           using FileStream fileStream =
+                new(fullPath, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, false);
+
+            await file.CopyToAsync(fileStream);
+            await fileStream.FlushAsync();
         }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(string id)
-        {
-            await _productWriteRepository.RemoveAsync(id);
-            await _productWriteRepository.SaveAsync();
-
-            return Ok();
-        }
+        return Ok();
     }
 }
